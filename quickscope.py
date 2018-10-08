da415708b9de33a5b8ef8614ff99da27d027fb54
KobaDev <35046243+KobaDev@users.noreply.github.com>
2018-10-08 13:52:25 -0300

    Update quickscope.py

--- quickscope.py
+++ quickscope.py
@@ -3,6 +3,7 @@ from tkinter import Frame, Label, ttk
 from os import popen
 
 def chCpu():
+
     command = "grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}'"
     cpuUsage = str(round(float(popen(command).read()), 3))
 
@@ -14,6 +15,7 @@ def chCpu():
     root.after(1000, chCpu)
 
 def chHDD(event):
+
     part = lstBx.get()
     command = 'df ' + part
     hddUsagelist = list(popen(command).readlines())
@@ -28,7 +30,11 @@ def chHDD(event):
 
     leftHdd.configure(text='Left space:\n' + str(round(hddLeft / 1000000, 3)) + ' GB')
 
+
+
+
 def chRam():
+
     meminfo = dict((i.split()[0].rstrip(':'), int(i.split()[1])) for i in open('/proc/meminfo').readlines())
 
     mem_tt_kib = meminfo['MemTotal']
@@ -41,10 +47,11 @@ def chRam():
     mem_av_gib= str(round(mem_av_kib/1048576, 3))
 
     TtRam.configure(text='Total RAM:\n'+mem_tt_gib+' GB')
-    AcRam.configure(text='Total RAM:\n'+mem_ac_gib+' GB')
-    AvRam.configure(text='Total RAM:\n'+mem_av_gib+' GB')
+    AcRam.configure(text='Active RAM:\n'+mem_ac_gib+' GB')
+    AvRam.configure(text='Available RAM:\n'+mem_av_gib+' GB')
 
     root.after(1000, chRam)
+
 
 root = tk.Tk()
 h, w = 600, 350
@@ -66,6 +73,7 @@ frmContainer.pack()
 
 #-----------#
 
+
 frmCpu = Frame(frmContainer, height=250, width=200, background='#505050', highlightbackground='#404040', highlightthickness=1)
 frmCpu.pack_propagate(False)
 frmCpu.grid(column=0, row=0)
@@ -80,6 +88,7 @@ tempCpu = Label(frmCpu, text='', foreground='white', background='#505050')
 tempCpu.pack()
 
 #-----------#
+
 
 frmHDD = Frame(frmContainer, height=250, width=200, background='#505050', highlightbackground='#404040', highlightthickness=1)
 frmHDD.pack_propagate(False)
@@ -147,4 +156,5 @@ AvRam.pack(pady=(0,3))
 
 chCpu()
 chRam()
+
 root.mainloop()

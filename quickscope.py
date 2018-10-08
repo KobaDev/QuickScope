2ff0f685c00f78159e05b5cdfde2feafa9a31471
KobaDev <35046243+KobaDev@users.noreply.github.com>
2018-10-08 13:44:33 -0300

    Update for quickscope.py
    Features:
    - Code optimisation.
    - Added features for drive section.

--- quickscope.py
+++ quickscope.py
@@ -1,81 +1,50 @@
 import tkinter as tk
-from tkinter import Frame, Label
+from tkinter import Frame, Label, ttk
 from os import popen
 
 def chCpu():
-    for widget in frmCpu.winfo_children():
-        widget.destroy()
-
-    titleCpu = Label(frmCpu, font=("Arial bold", 18), text='CPU', background='#505050', foreground='white')
-    titleCpu.pack(side='top', pady=(20, 30))
-
     command = "grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}'"
-    cpuUsage = str(popen(command).read())
+    cpuUsage = str(round(float(popen(command).read()), 3))
 
     tempInfo = str(open('/sys/class/thermal/thermal_zone0/temp').readlines())
 
-    usgCpu = Label(frmCpu, text=cpuUsage[:-1]+'%', foreground='white', background='#505050')
-    usgCpu.pack()
-
-    tempCpu = Label(frmCpu, text=tempInfo[2:4]+'.'+tempInfo[5]+'ºC', foreground='white', background='#505050')
-    tempCpu.pack()
+    usgCpu.configure(text='Total usage:\n' + cpuUsage[:-1]+'%')
+    tempCpu .configure(text='Temperature:\n' + tempInfo[2:4]+'.'+tempInfo[5]+'ºC')
 
     root.after(1000, chCpu)
 
-def chHDD():
-    for widget in frmHDD.winfo_children():
-        widget.destroy()
-
-    titleHDD = Label(frmHDD, font=("Arial bold", 18), text='Disk', background='#505050', foreground='white')
-    titleHDD.pack(side='top', pady=(20, 30))
-
-    command = 'df /dev/sda6'
+def chHDD(event):
+    part = lstBx.get()
+    command = 'df ' + part
     hddUsagelist = list(popen(command).readlines())
     hddUsage = hddUsagelist[1].split()
     hddTotal = float(hddUsage[1])
     hddUsed = float(hddUsage[2])
     hddLeft = float(hddUsage[3])
 
-    ttlHdd = Label(frmHDD, text='Total space:\n'+str(round(hddTotal/1000000, 3))+' GB', foreground='white', background='#505050')
-    ttlHdd.pack(pady=(0,10))
+    ttlHdd.configure(text='Total space:\n' + str(round(hddTotal / 1000000, 3)) + ' GB')
 
-    usdHdd = Label(frmHDD, text='Used space:\n'+str(round(hddUsed/1000000, 3))+' GB', foreground='white', background='#505050')
-    usdHdd.pack(pady=(0,10))
+    usdHdd.configure(text='Used space:\n' + str(round(hddUsed / 1000000, 3)) + ' GB')
 
-    leftHdd = Label(frmHDD, text='Left space:\n'+str(round(hddLeft/1000000, 3))+' GB', foreground='white', background='#505050')
-    leftHdd.pack()
-
-    root.after(1000, chHDD)
+    leftHdd.configure(text='Left space:\n' + str(round(hddLeft / 1000000, 3)) + ' GB')
 
 def chRam():
-    for widget in frmRam.winfo_children():
-        widget.destroy()
-
-    titleRam = Label(frmRam, font=("Arial bold", 18), text='RAM', background='#505050', foreground='white')
-    titleRam.pack(side='top', pady=(20, 30))
-
     meminfo = dict((i.split()[0].rstrip(':'), int(i.split()[1])) for i in open('/proc/meminfo').readlines())
 
     mem_tt_kib = meminfo['MemTotal']
-    mem_tt_gib= mem_tt_kib/1048576
+    mem_tt_gib= str(round(mem_tt_kib/1048576, 3))
 
     mem_ac_kib = meminfo['Active']
-    mem_ac_gib= mem_ac_kib/1048576
+    mem_ac_gib= str(round(mem_ac_kib/1048576, 3))
 
     mem_av_kib = meminfo['MemAvailable']
-    mem_av_gib= mem_av_kib/1048576
+    mem_av_gib= str(round(mem_av_kib/1048576, 3))
 
-    TtRam = Label(frmRam, text='Total: '+str(round(mem_tt_gib, 2))+' GB', foreground='white', background='#505050')
-    TtRam.pack(pady=(0,3))
-
-    AcRam = Label(frmRam, text='Active: '+str(round(mem_ac_gib, 2))+' GB', foreground='white', background='#505050')
-    AcRam.pack(pady=(0,3))
-
-    AvRam = Label(frmRam, text='Available: '+str(round(mem_av_gib, 2))+' GB', foreground='white', background='#505050')
-    AvRam.pack(pady=(0,3))
+    TtRam.configure(text='Total RAM:\n'+mem_tt_gib+' GB')
+    AcRam.configure(text='Total RAM:\n'+mem_ac_gib+' GB')
+    AvRam.configure(text='Total RAM:\n'+mem_av_gib+' GB')
 
     root.after(1000, chRam)
-
 
 root = tk.Tk()
 h, w = 600, 350
@@ -95,20 +64,87 @@ subtitle.pack()
 frmContainer = Frame(root, height=700, width=600)
 frmContainer.pack()
 
+#-----------#
+
 frmCpu = Frame(frmContainer, height=250, width=200, background='#505050', highlightbackground='#404040', highlightthickness=1)
 frmCpu.pack_propagate(False)
 frmCpu.grid(column=0, row=0)
+
+titleCpu = Label(frmCpu, font=("Arial bold", 18), text='CPU', background='#505050', foreground='white')
+titleCpu.pack(side='top', pady=(20, 30))
+
+usgCpu = Label(frmCpu, text='', foreground='white', background='#505050')
+usgCpu.pack()
+
+tempCpu = Label(frmCpu, text='', foreground='white', background='#505050')
+tempCpu.pack()
+
+#-----------#
 
 frmHDD = Frame(frmContainer, height=250, width=200, background='#505050', highlightbackground='#404040', highlightthickness=1)
 frmHDD.pack_propagate(False)
 frmHDD.grid(column=1, row=0)
 
+titleHDD = Label(frmHDD, font=("Arial bold", 18), text='Disk', background='#505050', foreground='white')
+titleHDD.pack(side='top', pady=(20, 5))
+
+partition = Label(frmHDD, text='Section:', background='#505050', foreground='white')
+partition.pack(pady=(0, 5))
+
+hddParts = list(popen('df').readlines())
+hddPartsLen = len(hddParts)
+
+partitionlist = []
+
+for x in range(1, hddPartsLen):
+    hddPartition = hddParts[x].split()
+    if hddPartition[0] != 'tmpfs' and hddPartition[0] != 'udev' :
+        partitionlist.append(hddPartition[0])
+
+lstBx = ttk.Combobox(frmHDD, width=15)
+lstBx['values'] = partitionlist
+lstBx.set(partitionlist[0])
+lstBx.pack(pady=(0, 20))
+
+lstBx.bind("<<ComboboxSelected>>", chHDD)
+
+part = lstBx.get()
+command = 'df ' + part
+hddUsagelist = list(popen(command).readlines())
+hddUsage = hddUsagelist[1].split()
+hddTotal = round(float(hddUsage[1]) / 1000000, 3)
+hddUsed = round(float(hddUsage[2]) / 1000000, 3)
+hddLeft = round(float(hddUsage[3])/ 1000000, 3)
+
+ttlHdd = Label(frmHDD, text='Total space:\n' + str(hddTotal) + ' GB', foreground='white', background='#505050')
+ttlHdd.pack(pady=(0, 10))
+
+usdHdd = Label(frmHDD, text='Used space:\n' + str(hddUsed) + ' GB', foreground='white', background='#505050')
+usdHdd.pack(pady=(0, 10))
+
+leftHdd = Label(frmHDD, text='Left space:\n' + str(hddLeft) + ' GB', foreground='white', background='#505050')
+leftHdd.pack()
+
+#-----------#
+
 frmRam = Frame(frmContainer, height=250, width=200, background='#505050', highlightbackground='#404040', highlightthickness=1)
 frmRam.pack_propagate(False)
 frmRam.grid(column=2, row=0)
 
-chCpu()
-chHDD()
-chRam()
+titleRam = Label(frmRam, font=("Arial bold", 18), text='RAM', background='#505050', foreground='white')
+titleRam.pack(side='top', pady=(20, 30))
 
+TtRam = Label(frmRam, text='', foreground='white', background='#505050')
+TtRam.pack(pady=(0,3))
+
+AcRam = Label(frmRam, text='', foreground='white', background='#505050')
+AcRam.pack(pady=(0,3))
+
+AvRam = Label(frmRam, text='', foreground='white', background='#505050')
+AvRam.pack(pady=(0,3))
+
+#----------#
+
+chCpu()
+chRam()
 root.mainloop()
